15549f8c711ecd141d795bbb86a2ad6ea73d783a
Arya Priema <99aryaprima@gmail.com>
2024-05-09 23:04:06 +0700

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,160 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import Review from '@/components/Review';
+import { Progress } from '@/components/ui/progress';
+import { useUploadThing } from '@/lib/uploadthing';
+import { cn } from '@/lib/utils';
+import { Image, Loader2, MousePointerSquareDashed } from 'lucide-react';
+import { useState, useTransition } from 'react';
+import Dropzone, { FileRejection } from 'react-dropzone';
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function Page() {
+	const [isDragOver, setIsDragOver] = useState<boolean>(false);
+	const [uploadProgress, setUploadProgress] = useState<number>(0);
+	const [isPending, startTransition] = useTransition();
+	const [isOpen, setIsOpen] = useState<boolean | null>(null);
+	const [url, setUrl] = useState<string | null>(null);
+	const [preview, setPreview] = useState<string | null>(null);
+	const [files, setFiles] = useState<File[] | null>(null);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const { startUpload, isUploading } = useUploadThing('imageUploader', {
+		onClientUploadComplete: ([data]) => {
+			const urlImg = data.serverData.url;
+			startTransition(() => {
+				// You can call a function, for example if you want to save it to the database
+				// but in this case I save it into state
+				setUrl(urlImg);
+			});
+		},
+		onUploadProgress: (p) => {
+			setUploadProgress(p);
+		},
+	});
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const onDropRejected = (rejectedFiles: FileRejection[]) => {
+		const [file] = rejectedFiles;
+		setIsDragOver(false);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		console.error({
+			title: `${file.file.type} type is not suppported.`,
+			description: 'Please choose a PNG, JPG, or JPEG image instead.',
+			variant: 'destructive',
+		});
+	};
+	const onDropAccepted = (acceptedFiles: File[]) => {
+		// startUpload(acceptedFiles);
+		// setIsDragOver(false);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+		const file: File = acceptedFiles[0];
+		if (file) {
+			setPreview(URL.createObjectURL(file));
+			setIsOpen(true);
+			setFiles(acceptedFiles);
+		}
+
+		setIsDragOver(false);
+	};
+
+	return (
+		<>
+			<div className='h-full flex items-center justify-center flex-col gap-y-6'>
+				<h1 className='relative tracking-tight text-balance font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl text-center'>
+					Upload
+					<span className='bg-red-600 px-2 text-white'>Thing</span>
+				</h1>
+				<div className='mx-auto w-full max-w-4xl h-[500px]'>
+					<div
+						className={cn(
+							'relative h-full flex-1 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center',
+							{
+								'ring-blue-900/25 bg-blue-900/10': isDragOver,
+							}
+						)}
+					>
+						<div className='relative flex flex-1 flex-col items-center justify-center w-full'>
+							<Dropzone
+								onDropRejected={onDropRejected}
+								onDropAccepted={onDropAccepted}
+								accept={{
+									'image/png': ['.png'],
+									'image/jpeg': ['.jpeg'],
+									'image/jpg': ['.jpg'],
+								}}
+								onDragEnter={() => setIsDragOver(true)}
+								onDragLeave={() => setIsDragOver(false)}
+							>
+								{({ getRootProps, getInputProps }) => (
+									<div
+										className='h-full w-full flex-1 flex flex-col items-center justify-center'
+										{...getRootProps()}
+									>
+										<input
+											hidden
+											{...getInputProps}
+										/>
+										{isDragOver ? (
+											<MousePointerSquareDashed className='h-6 w-6 text-zinc-500 mb-2' />
+										) : isUploading || isPending ? (
+											<Loader2 className='animate-spin h-6 w-6 text-zinc-500 mb-2' />
+										) : (
+											<Image className='h-6 w-6 text-zinc-500 mb-2' />
+										)}
+										<div className='flex flex-col justify-center mb-2 text-sm text-zinc-700'>
+											{isUploading ? (
+												<div className='flex flex-col items-center'>
+													<p>Uploading...</p>
+													<Progress
+														value={uploadProgress}
+														className='mt-2 w-40 h-2 bg-gray-300'
+													/>
+												</div>
+											) : isPending ? (
+												<div className='flex flex-col items-center'>
+													<p>Please wait...</p>
+												</div>
+											) : isDragOver ? (
+												<p>
+													<span className='font-semibold'>
+														Drop file
+													</span>{' '}
+													to upload
+												</p>
+											) : (
+												<p>
+													<span className='font-semibold'>
+														Click to upload
+													</span>{' '}
+													or drag and drop
+												</p>
+											)}
+										</div>
+
+										{isPending ? null : (
+											<p className='text-xs text-zinc-500'>
+												PNG, JPG, JPEG
+											</p>
+										)}
+									</div>
+								)}
+							</Dropzone>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{isOpen && (
+				<Review
+					startUpload={startUpload}
+					setPreview={setPreview}
+					preview={preview}
+					isOpen={isOpen}
+					setIsOpen={setIsOpen}
+					isPending={isPending}
+					files={files}
+				/>
+			)}
+		</>
+	);
 }
+
+export default Page;
